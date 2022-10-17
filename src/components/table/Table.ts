c9663f2f9610e08ca9c7717345f93f3b88a8f423
Aleksandr <aleksandera9@gmail.com>
2022-10-17 05:00:41 +0300

    clear comments table

--- src/components/table/Table.ts
+++ src/components/table/Table.ts
@@ -24,42 +24,6 @@ export class Table extends ExcelComponent {
 		if(shouldResize(event)) {
 			resizeHandler(this.$root, event);
 		}
-		// 	const $resizer = $(event.target as HTMLElement);
-		// 	const $parent = $resizer.closest("[data-type=\"resizable\"]");
-		// 	const coords = $parent.getCoords();
-		// 	const resizerType = $resizer.data.resize;
-		// 	const sideProp = resizerType === "col" ? "bottom" : "right";
-		// 	let value: number;
-		//
-		// 	$resizer.css({opacity: "1", [sideProp]: "-5000px"});
-		//
-		// 	document.onmousemove = e => {
-		// 		if(resizerType === "col") {
-		// 			const delta = e.pageX - coords.right;
-		// 			value = coords.width + delta;
-		// 			$resizer.css({right: -delta + "px"});
-		//
-		// 		} else {
-		// 			const delta = e.pageY - coords.bottom;
-		// 			value = coords.height + delta;
-		// 			$resizer.css({bottom: -delta + "px"});
-		// 		}
-		// 	};
-		//
-		// 	document.onmouseup = e => {
-		// 		document.onmousemove = null;
-		// 		document.onmouseup = null;
-		// 		if(resizerType === "col") {
-		// 			$parent.css({width: value + "px"});
-		// 			this.$root.findAll(`[data-col="${$parent.data.col}"]`)
-		// 				.forEach((el: Node): string => (<HTMLDivElement>el).style.width = value + "px" );
-		// 		} else {
-		// 			$parent.css({height: value + "px"});
-		// 		}
-		// 		$resizer.css({opacity: "0", bottom: "0", right: "0"});
-		// 	};
-		// }
-
 	}
 
 	onMousemove(event: Event): void {
